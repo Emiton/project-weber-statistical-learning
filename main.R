c58453ab1085ed0353da9d3d07a6f1c4f7c33ab6
Emiton Alves <emiton_alves@my.uri.edu>
2019-03-26 09:12:30 -0400

    Updated column names

--- main.R
+++ main.R
@@ -22,8 +22,22 @@ testable_data <- data.frame(data$`Client Number`, data$`1`, data$`5`,
                             data$`8`, data$`10`, data$`13`,
                             data$`27`, data$`28`, data$`11`,
                             data$`35`, data$`37`, data$`38`)
+# Rename columns
+testable_data <- testable_data %>%
+  rename(
+    age = data..1.,
+    schooling = data..5.,
+    housing = data..8.,
+    jail = data..10.,
+    health_care = data..13.,
+    domestic_abuse = data..27.,
+    career_duration = data..28.,
+    female_partners = data..35.,
+    male_partners = data..37.,
+    unprotected_male = data..38.
+  )
 
-
+colnames(testable_data)
 # collapse all drug use cases into single column
 testable_data$drug_use <- data$`30A` + data$`30B` + data$`30C`
                            + data$`30D`+ data$`30E`+ data$`30F`
@@ -45,14 +59,13 @@ pie(housing)
 myBar <- ggplot(testable_data, aes(testable_data$data..8.)) + geom_bar(fill = "#0073C2FF")
 
 # histogram with many different bars
-a <- ggplot(testable_data, aes(x = testable_data$data..1.))
-a + geom_histogram(bins = 30, color = "black", fill = "gray")
+a <- ggplot(testable_data, aes(x = testable_data$data..5.))
+a + geom_histogram(bins = 5, color = "black", fill = "gray")
 
-# density plot
+# density plot with dotted line for mean
 b <- ggplot(testable_data, aes(x = testable_data$data..1.))
-b + geom_density()
+b + geom_density() +geom_vline(aes(xintercept = mean(testable_data$data..1.)), linetype = "dashed", size = 0.6) + ggtitle("Something NEw") +
+    labs(x="My loorrrrt")
 
-# pie chart for one column ??
-# data spead for one feature ??
 
 summary(testable_data)
